48d95fc1b2cbf4c5f4d512ca4146d32038b65d09
etniqa <baranovstyopa@gmail.con>
2020-02-15 14:14:54 +0200

    with composition

--- src/main/java/edu/stev/cursach/model/Student.java
+++ src/main/java/edu/stev/cursach/model/Student.java
@@ -2,11 +2,13 @@ package edu.stev.cursach.model;
 
 import java.time.LocalDateTime;
 
-public class Group {
+public class Student {
     //necessary field in every class
     private String id;
     private String name;
     private String description;
     private LocalDateTime creationDate;
     private LocalDateTime dateModified;
+
+    private Group group;
 }
