7a9de0c78b1d84a2826abc0739c2e7884c7cf2c7
etniqa <baranovstyopa@gmail.con>
2020-02-15 13:57:06 +0200

    Initial commit

---
+++ src/main/java/edu/stev/cursach/CursachApplication.java
@@ -0,0 +1,13 @@
+package edu.stev.cursach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursachApplication.class, args);
+    }
+
+}
---
+++ src/main/java/edu/stev/cursach/model/Group.java
@@ -0,0 +1,6 @@
+package edu.stev.cursach.model;
+
+public class Group {
+    private String id;
+    private String name;
+}
