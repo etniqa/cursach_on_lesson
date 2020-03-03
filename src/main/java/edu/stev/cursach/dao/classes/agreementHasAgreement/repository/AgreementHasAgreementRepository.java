da7794e18461d1783681498bcaf1e63bbd7a07ba
etniqa <etniqaaa@gmail.com>
2020-03-03 09:36:12 +0200

    daf

--- src/main/java/edu/stev/cursach/dao/classes/agreementHasAgreement/repository/AgreementHasAgreementRepository.java
+++ src/main/java/edu/stev/cursach/dao/classes/agreementHasAgreement/repository/AgreementHasAgreementRepository.java
@@ -3,5 +3,5 @@ package edu.stev.cursach.dao.classes.agreementHasAgreement.repository;
 import edu.stev.cursach.model.AgreementHasAgreement;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface AgreementHasAgreementRepository extends MongoRepository<AgreementHasAgreement, String> {
-}
+//public interface AgreementHasAgreementRepository extends MongoRepository<AgreementHasAgreement, String> {
+//}
